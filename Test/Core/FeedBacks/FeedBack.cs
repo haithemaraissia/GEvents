b360953b0ba01c19c5ec4f64772c9d479fd0f9ac
haithemaraissia <haithemaraissia@gmail.com>
2015-12-16 10:49:45 -0700

    Cleaning Up Code

--- Test/Core/FeedBacks/FeedBack.cs
+++ Test/Core/FeedBacks/FeedBack.cs
@@ -30,18 +30,18 @@ namespace Test.Core.FeedBacks
         [TestMethod]
         public void FeedBackRateStatistics()
         {
-            var feedbackgroups = _uow.FeedBackRepository.All.GroupBy(x => new {x.SectionId })
+            var feedbackgroups = _uow.FeedBackRepository.All.GroupBy(x => new { x.SectionId })
                 .Select(g => new FeedbackStatistics
                 {
                     Average = Math.Round(g.Average(p => p.RateId), 2),
                     SectionId = g.Key.SectionId,
-                    Percentage = Math.Round((g.Average(p => p.RateId) * 100)/(int)RateCode.Values.VeryGood,2),
+                    Percentage = Math.Round((g.Average(p => p.RateId) * 100) / (int)RateCode.Values.VeryGood, 2),
                     SectionName = GetSectionNameBySectionId(g.Key.SectionId),
                     NumberOfRaters = g.Count()
                 });
 
-            Assert.AreEqual(3,feedbackgroups.Count());
-            var eventSection = feedbackgroups.FirstOrDefault(x => x.SectionId == (int) EventRateSection.Values.Events);
+            Assert.AreEqual(3, feedbackgroups.Count());
+            var eventSection = feedbackgroups.FirstOrDefault(x => x.SectionId == (int)EventRateSection.Values.Events);
             if (eventSection != null)
             {
                 Assert.AreEqual(3.67, eventSection.Average);
@@ -50,13 +50,15 @@ namespace Test.Core.FeedBacks
             }
         }
 
-        private string GetSectionNameBySectionId(int sectionId )
+        #region Util
+        private string GetSectionNameBySectionId(int sectionId)
         {
-            var eventRateSection = _uow.EventRateSectionRepository.FindBy(x=>x.SectionId == sectionId).FirstOrDefault();
+            var eventRateSection = _uow.EventRateSectionRepository.FindBy(x => x.SectionId == sectionId).FirstOrDefault();
             if (eventRateSection != null)
                 return eventRateSection.SectionValue.ToString();
             return "section Name";
         }
+        #endregion
 
     }
 }
